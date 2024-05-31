f711ead00d631f8b450ab10d569695f9f16de082
Logik-front-end <viktorsusorov01@gmail.com>
2024-05-31 19:22:03 +0300

    idk1

--- src/servises/component/work/work.js
+++ src/servises/component/work/work.js
@@ -1,9 +1,16 @@
 import React from "react";
 import './work.css'
+import img1 from "../../img/site.png"
 
 function work() {
   return (
-     <div></div>
+     <div className="work"> 
+     <h1>My works</h1>
+      <a href="https://midnight-bar.netlify.app"><img src={img1} alt="a"/></a>
+      <a href="https://midnight-bar.netlify.app"><img src={img1} alt="a"/></a>
+      <a href="https://midnight-bar.netlify.app"><img src={img1} alt="a"/></a>
+      <a href="https://midnight-bar.netlify.app"><img src={img1} alt="a"/></a>
+     </div>
   );
 }
 
