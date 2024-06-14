676a3077dfc42557c7a7724b3ae230c2d28d368a
Logik-front-end <Viktorsusorov01@gmail.com>
2024-06-14 19:40:25 +0300

    JAbka

--- src/servises/component/main/about me/about.js
+++ src/servises/component/main/about me/about.js
@@ -100,14 +100,23 @@ const About = () => {
     return (
         <div className="about">
             <h1>About Me</h1>
+            <h3>this text is draggable</h3>
             <div className="text" onDrop={handleDrop} onDragOver={handleDragOver}>
+                <div className="mobile">
+                    <p>I am a web developer with 3 years of experience.I specialize in creative
+                    interactive and responsive web applications,</p>
+                    <p>using advanced technologies and approaches. My passion for programming began I started learning
+                        the front-end since </p>
+                    <p>the time of the Crusades,this is Friedrich 2's advice to me, and I decided to learn the
+                        front-end, </p>
+                    <p>and since then I have been steadily developing in this area.</p></div>
                 {words.map((word, index) => (
                     <span
                         key={index}
                         draggable
                         onDragStart={(e) => handleDragStart(e, index)}
                         className="draggable-word"
-                        style={{ left: word.x, top: word.y }}
+                        style={{left: word.x, top: word.y}}
                     >
                         {word.text}{' '}
                     </span>
